83b9c2ede3dff242f0cd23bdb14ba30d1bf146ad
Austin Lubetkin <bocaaust@gmail.com>
2017-05-11 18:38:03 -0400

    Finish Quiz

--- BacteriaFormationTutor/BacteriaFormationTutor.codea/Main.lua
+++ BacteriaFormationTutor/BacteriaFormationTutor.codea/Main.lua
@@ -2,15 +2,45 @@
 displayMode(FULLSCREEN_NO_BUTTONS)
 -- Use this function to perform your initial setup
 function setup()
+    rectMode(CENTER)
     debugDraw = PhysicsDebugDraw()
     ellipseMode(CENTER)
     xw = WIDTH/1024
+    if xw >= 1 then
+        xw = 0.8
+    end
+    if HEIGHT/WIDTH < xw then
+        xw = HEIGHT/WIDTH*0.8
+    end
     cBody = 0
     touchMap = {}
     label = ""
     tutorial = false
     choices = {"diplococci","diplobacilli","streptococci","streptobacilli","staphylococci","staphylobacilli","tetrad","sarcina"}
-    quiz = {false}
+--1: Is it in quiz screen, 2: is quizzing
+    quiz = {false,false}
+end
+
+function orientationChanged( newOrientation )
+    xw = WIDTH/1024
+    if xw >= 1 then
+        xw = 0.8
+    end
+    if HEIGHT/WIDTH < xw then
+        xw = HEIGHT/WIDTH*0.8
+    end
+end
+
+function choiceCheck(input)
+    output = (input == label)
+    if #quiz > 3 and output == false then
+        for j=3,#quiz do
+            if quiz[j] == input then
+                output = true
+            end
+        end
+    end
+    return output
 end
 
 -- This function gets called once every frame
@@ -20,18 +50,47 @@ function draw()
     fill(0)
     if tutorial then
         sprite("Project:tutorial",WIDTH/2,HEIGHT/2,WIDTH*3/4)
+    elseif quiz[1] then
+        if quiz[3] == nil then
+            for i=3,6 do
+                temp = math.random(1,#choices)
+                while choiceCheck(choices[temp]) do
+                     temp = math.random(1,#choices)
+                end
+                quiz[i] = choices[temp]
+            end
+            quiz[math.random(3,6)] = label
+        end
+        debugDraw:draw()
+        fontSize(70*xw)
+        text("What is this formation?",WIDTH/2,HEIGHT*7/8)
+        for i=3,6 do
+            --tint(255)
+            fill(0,90,255,125)
+            strokeWidth(0)
+            rect(WIDTH/2,HEIGHT*7.3/8-(i-2)*HEIGHT/5,WIDTH*7/8,HEIGHT/6)
+            fill(0)
+            --sprite("Project:box",WIDTH/2,HEIGHT*7/8-(i-2)*HEIGHT/5,WIDTH*7/8,HEIGHT/6)
+            text(quiz[i],WIDTH/2,HEIGHT*7.3/8-(i-2)*HEIGHT/5)
+        end
     else
         debugDraw:draw()
-        sprite("Project:coc",WIDTH/4,HEIGHT/8,100*xw)
+        spriteMode(CENTER)
+        sprite("Project:coc",WIDTH/5,HEIGHT/8,100*xw)
         -- ellipse(WIDTH/4,HEIGHT/8,100*xw)
-        sprite("Project:bac",WIDTH*3/4,HEIGHT/8,100*xw,200*xw)
+        sprite("Project:bac",WIDTH*3/5,HEIGHT/8,100*xw,200*xw)
         --ellipse(WIDTH*3/4,HEIGHT/8,100*xw,200*xw)
         fontSize(100*xw)
-        text("✖️",WIDTH/2,HEIGHT/8)
+        text("✖️",WIDTH*2/5,HEIGHT/8)
+        if quiz[2] then
+        fill(0,255,0,255)
+        else
+        fill(255,0,0,255)
+        end
+        text("?",WIDTH*4/5,HEIGHT/8)
+        fill(0)
         
-        
-        
-        text(label, WIDTH/2,HEIGHT/4)
+        text(label, WIDTH/2,HEIGHT/3.6)
         
         if CurrentTouch.state == ENDED then
         detection()
@@ -57,39 +116,59 @@ function detection()
     if #debugDraw.joints == 1 then
         if label ~= "diplo"..debugDraw.joints[#debugDraw.joints].bodyA.info then
             label = "diplo"..debugDraw.joints[#debugDraw.joints].bodyA.info
+            if quiz[2] then
+                quiz[1] = true
+            else
             if debugDraw.joints[#debugDraw.joints].bodyA.info == "cocci" then
                 speech.say("dip low cox eye")
             else
                 speech.say("dip low bass sill eye")
             end
+            end
         end
     elseif strepCheck() then
         if label ~="strepto"..debugDraw.joints[#debugDraw.joints].bodyA.info then
             label ="strepto"..debugDraw.joints[#debugDraw.joints].bodyA.info
+if quiz[2] then
+quiz[1] = true
+else
             if debugDraw.joints[#debugDraw.joints].bodyA.info == "cocci" then
                 speech.say("strehp toe cox eye")
             else
                 speech.say("strep toe bass sill eye")
             end
+            end
         end
     elseif staphCheck() then
         if label ~="staphylo"..debugDraw.joints[#debugDraw.joints].bodyA.info then
             label ="staphylo"..debugDraw.joints[#debugDraw.joints].bodyA.info
+if quiz[2] then
+quiz[1] = true
+else
             if debugDraw.joints[#debugDraw.joints].bodyA.info == "cocci" then
                 speech.say("staff ill low cox eye")
             else
                 speech.say("staff ill low bass sill eye")
             end
+end
         end
     elseif tetCheck() then
         if label ~= "tetrad" then
+if quiz[2] then
+quiz[1] = true
+else
             speech.say("teh trahd")
+end
             label = "tetrad"
         end
     elseif sarCheck() then
         if label ~= "sarcina" then
             label = "sarcina"
+if quiz[2] then
+quiz[1] = true
+else
             speech.say("sahr sih knee")
+end
         end
     else
         label = ""
@@ -209,18 +288,34 @@ end
 
 
 function touched(touch)
+    if quiz[1] then
+        if touch.state == BEGAN then
+            for i=3,6 do
+               if touch.y > HEIGHT*7.3/8-(i-2)*HEIGHT/5-HEIGHT/12 and touch.y < HEIGHT*7.3/8-(i-2)*HEIGHT/5+HEIGHT/12 then
+                    if quiz[i] == label then
+                        quiz[1] = false
+                        speech.say("correct!")
+                    else
+                        speech.say("try again")
+                    end
+                end
+            end
+        end
+    else
     flag = false
     if touch.state == BEGAN then
         if touch.y > HEIGHT/8-100*xw and touch.y < HEIGHT/8+100*xw then
-            if touch.x > WIDTH/4-100*xw and touch.x < WIDTH/4+100*xw  then
+            if touch.x > WIDTH/5-100*xw and touch.x < WIDTH/5+100*xw  then
                 createCircle(WIDTH/4,HEIGHT/8,100*xw,"cocci")
                 touchMap[touch.id] = cBody
-            elseif touch.x > WIDTH/2-100*xw and touch.x < WIDTH/2+100*xw then
+            elseif touch.x > WIDTH*2/5-100*xw and touch.x < WIDTH/5*2+100*xw then
                 debugDraw:clear()
                 touchMap = {}
-            elseif touch.x > WIDTH/4*3-100*xw and touch.x < WIDTH/4*3+100*xw then
+            elseif touch.x > WIDTH/5*3-100*xw and touch.x < WIDTH/5*3+100*xw then
                 createBox(WIDTH/4,HEIGHT/8,100*xw,200*xw,"bacilli")
                 touchMap[touch.id] = cBody
+            elseif touch.x > WIDTH/5*4-100*xw and touch.x < WIDTH/5*4+100*xw then
+                quiz[2] = (quiz[2] == false)
             end
         elseif touch.y > HEIGHT/8*7-40*xw then
             tutorial = true
@@ -237,7 +332,7 @@ function touched(touch)
         debugDraw.bodies[touchMap[touch.id]].x = touch.x
         debugDraw.bodies[touchMap[touch.id]].y = touch.y
     end
-    
+    end
 end
 
 function detectTouch(touch)
